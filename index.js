632fc270a5656bfd7e43284d2857cd5d2acc583a
whisperstars <whisperstars@users.noreply.github.com>
2018-07-03 00:16:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 app.listen(8080, () => {
   console.log('server started')
 });
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/write', function (req, res) {
+  res.send(fs.writeFileSync('/data/a.txt', 'hello World'));
+  res.send('writed to file');
+});
+
+app.get('/read', function (req, res) {
+  res.send(fs.readFileSync('/data/a.txt', 'UTF8'));
 });
